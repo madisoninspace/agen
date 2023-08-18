8543cd751e9555fbcf64a009ad759f6bc3ed33e9
Hannah Wass <931615+hannahwxyz@users.noreply.github.com>
2023-08-17 21:27:01 -0400

    Way downloading and parsing

--- src/agen.ts
+++ src/agen.ts
@@ -1,4 +1,6 @@
 import { Command } from 'commander';
+import { downloadWay } from './osm/downloadWay.js';
+import { parse } from './osm/parse.js';
 import { version } from './version.js';
 
 const program = new Command();
@@ -6,5 +8,17 @@ program
     .name('agen')
     .version(version)
     .description('Tool to create aprons for MSFS scenery using OpenStreetMap data.');
+
+program
+    .command('remote <id>')
+    .description('Download a way from OpenStreetMap API.')
+    .action(async (id: string) => {
+        console.log('Downloading way #' + id);
+
+        const way = await downloadWay(id);
+        const coords = await parse(way);
+
+        console.log(coords);
+    });
 
 program.parse();
